2d9bbb1ca9a28305676298a1ee1cc485522e6b86
s.zyuban <s.zyuban@max.credit>
2021-12-10 12:01:57 +0300

    todo

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,7 +9,8 @@ class MainController < ApplicationController
     records = helpers.get_records_from_db(client.query('SELECT * FROM `max-credit`.fssp_check'))
 
     rest_client = RestClient
-    rest_client.proxy = 'localhost:8118'
+    port = 8118
+    rest_client.proxy = "localhost:#{port}"
     i = 1
     token = Token.find(i).value
     request_count = 0
@@ -24,39 +25,43 @@ class MainController < ApplicationController
             'birthdate' => client['birthdate']
           }
         }
-      rescue rest_client::ExceptionWithResponse => e
+      rescue rest_client::ExceptionWithResponse
         exception_in_response = change_token(i)
         token = exception_in_response[:token]
         i = exception_in_response[:iterator]
-        sleep(10)
+        rest_client.proxy = "localhost:#{exception_in_response[:port]}"
+        # sleep(10)
         next
       end
 
       Response.create(result: JSON.parse(response)['response']['task'], token: token)
 
-      if request_count == 2
+      if request_count == 30
         exception_in_response = change_token(i)
         token = exception_in_response[:token]
         i = exception_in_response[:iterator]
-        rest_client.proxy = (rest_client.proxy == 'localhost:9050' ? 'localhost:8118' : 'localhost:9050')
+        # rest_client.proxy = exception_in_response[:port]
         request_count = 0
       end
       request_count += 1
       # sleep(rand(5))
     end
 
+    #todo get out to function
     Response.all.each do |client|
-      response = restClient.get 'https://api-ip.fssp.gov.ru/api/v1.0/status', { params: { 'token' => '04SlHONVuitv', 'task' => client.result } }
+      response = restClient.get 'https://api-ip.fssp.gov.ru/api/v1.0/status', 
+{ params: { 'token' => '04SlHONVuitv', 'task' => client.result } }
       client.status = JSON.parse(response)['response']['status']
       client.save
-      sleep(rand(12))
+      sleep(rand(5))
     end
 
     Response.all.each do |client|
-      response = restClient.get 'https://api-ip.fssp.gov.ru/api/v1.0/result', { params: { 'token' => '04SlHONVuitv', 'task' => client.result } }
+      response = restClient.get 'https://api-ip.fssp.gov.ru/api/v1.0/result', 
+{ params: { 'token' => '04SlHONVuitv', 'task' => client.result } }
       client.full_result = JSON.parse(response)['response']['status']
       client.save
-      sleep(12)
+      # sleep(12)
     end
 
     render json: Response.all
@@ -64,9 +69,15 @@ class MainController < ApplicationController
 
   def change_token(iterator)
     helpers.renew_ip
-    iterator += 1
-    iterator = 1 unless iterator < Token.count
 
-    { token: Token.find(iterator).value, iterator: iterator }
+    iterator += 1
+    iterator = 1 unless iterator <= Token.count
+
+    { token: Token.find(iterator).value, iterator: iterator,
+      port: %w[8118 7118 6118 5118 4118 3118 2118 1118 11118 21118][iterator] }
+  end
+
+  def renew_ip
+    helpers.renew_ip
   end
 end
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -20,9 +20,15 @@ module MainHelper
   end
 
   def renew_ip
-    localhost = Net::Telnet::new('Host' => 'localhost', 'Port' => '9051', 'Timeout' => 10, 'Prompt' => /250 OK\n/)
-    localhost.cmd('AUTHENTICATE ""') { |c| print c; throw 'Cannot authenticate to Tor' if c != "250 OK\n" }
-    localhost.cmd('signal NEWNYM') { |c| print c; throw 'Cannot switch Tor to new route' if c != "250 OK\n" }
-    localhost.close
+    [9051].each do |port|
+      localhost = Net::Telnet::new('Host' => 'localhost', 'Port' => port, 'Timeout' => 10, 'Prompt' => /250 OK\n/)
+      localhost.cmd('AUTHENTICATE ""') { |c| print c; throw 'Cannot authenticate to Tor' if c != "250 OK\n" }
+      localhost.cmd('signal NEWNYM') { |c| print c; throw 'Cannot switch Tor to new route' if c != "250 OK\n" }
+      localhost.cmd('setevents circ')
+      ex = localhost.cmd('getinfo stream-status') {
+        |c| print c
+      }
+      localhost.close
+    end
   end
 end
